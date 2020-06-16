9c25c311248897d4a895a19699b1cd6b80313267
Renanie Tanola <enan.tanola@gmail.com>
2020-06-15 23:41:21 -0700

    connected mongodb server

--- server.js
+++ server.js
@@ -3,6 +3,17 @@ const { ApolloServer } = require("apollo-server");
 const typeDefs = require("./typeDefs");
 const resolvers = require("./resolvers");
 
+const mongoose = require("mongoose");
+
+require("dotenv").config();
+
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+  })
+  .then(() => console.log("DB connected!"))
+  .catch((err) => console.error(err));
+
 new ApolloServer({
   typeDefs,
   resolvers,
