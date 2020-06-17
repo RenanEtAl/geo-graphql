75bb9a918919c1466ea5b31034ac03e498a0c057
Renanie Tanola <enan.tanola@gmail.com>
2020-06-16 21:10:42 -0700

    created models for users and pins

--- typeDefs.js
+++ typeDefs.js
@@ -1,14 +1,33 @@
-const { gql} = require('apollo-server')
+const { gql } = require("apollo-server");
 
 module.exports = gql`
-    type User {
-        _id: ID
-        name: String
-        email: String
-        picture: String
-    }
+  type User {
+    _id: ID
+    name: String
+    email: String
+    picture: String
+  }
 
-    type Query {
-        me: User
-    }
-`
+  type Pin {
+    _id: ID
+    createdAt: String
+    title: String
+    content: String
+    image: String
+    latitude: Float
+    longitude: Float
+    author: User
+    comments: [Comment]
+  }
+
+  type Comment {
+    text: String
+    createdAt: String
+    author: User
+  }
+
+  type Query {
+    me: User
+    getPins: [Pin!]
+  }
+`;
