ee141e1def0ad5095d2c3908af5d86ae48b9324f
Renanie Tanola <enan.tanola@gmail.com>
2020-06-24 18:31:09 -0700

    new color for newly created pins

--- client/src/components/map.components.js
+++ client/src/components/map.components.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect, useContext } from "react";
 import { withStyles } from "@material-ui/core/styles";
+
+import differenceInMinutes from "date-fns/difference_in_minutes";
 // import Button from "@material-ui/core/Button";
 // import Typography from "@material-ui/core/Typography";
 // import DeleteIcon from "@material-ui/icons/DeleteTwoTone";
@@ -61,6 +63,14 @@ const Map = ({ classes }) => {
     const { getPins } = await client.request(GET_PINS_QUERY);
     //console.log({ getPins });
     dispatch({ type: "GET_PINS", payload: getPins });
+  };
+
+  const highlightNewPin = (pin) => {
+    const isNewPin =
+      // current date, time the pin was created
+      differenceInMinutes(Date.now(), Number(pin.createdAt)) <= 30; // last 30 minutes
+
+    return isNewPin ? "limegreen" : "darkblue";
   };
   return (
     <div className={classes.root}>
